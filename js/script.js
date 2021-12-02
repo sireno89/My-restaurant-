8b6fb514a46cc97ec727ff2d9cd6b59bf7a1c15a
sireno89 <sireno89@gmail.com>
2021-12-02 11:58:53 +0200

    add loader

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
-/*-------- animation on scroll --------*/
+
 window.addEventListener('load', function () {
+  /*-------- page loader --------*/
+  document.querySelector('.page-loader').classList.add('fade-out');
+  setTimeout(function () {
+    document.querySelector('.page-loader').style.display='none';
+  },600);
+  /*-------- animation on scroll --------*/
   AOS.init();
 });
 
